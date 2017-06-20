12123cce3f4d6817a2036cc0adaac3ff741ae6c3
Julie Pierre <julie.pierre3@gmail.com>
2017-06-20 18:06:19 +0200

    page show en cours

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'flats#index'
 
-  resources :flats, only: [:index]
+  resources :flats, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
