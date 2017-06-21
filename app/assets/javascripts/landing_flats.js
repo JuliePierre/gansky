55b17e55b11dbde61a490221ab3f06dd72b50891
Julie Pierre <julie.pierre3@gmail.com>
2017-06-21 17:44:31 +0200

    page show / front bien avancé

--- app/assets/javascripts/landing_flats.js
+++ app/assets/javascripts/landing_flats.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#see-flats-button').click(function(event) {
     console.log('test');
-    var targetPosition = $('#landing-flats').offset().top;
+    var targetPosition = $('#landing-flats').offset().top - 50;
     $('html, body').animate({ scrollTop: targetPosition }, 'slow');
   })
 });
