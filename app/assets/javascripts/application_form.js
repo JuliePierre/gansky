2cf232ba86203c44d2cbefa0b4f02cc05d5c1d4b
Julie Pierre <julie.pierre3@gmail.com>
2017-06-22 18:16:02 +0200

    js du formulaire réglé, manque encore un peu de front mais quasi fini

--- app/assets/javascripts/application_form.js
+++ app/assets/javascripts/application_form.js
@@ -11,5 +11,20 @@ $('#show-step2-btn').click(function(){
     $('#submit-btn').slideDown();
     $('#show-step2-btn').css({"display": "none" });
     $('#footer').removeClass('full-bottom');
+  } else {
+    var $toastContent = $("<span>Vous ne pouvez pas valider des champs vides</span>");
+    Materialize.toast($toastContent, 5000);
   };
 });
+
+$('#submit-btn input').click(function(event){
+  if ($('#user_application_warrant_false').is(':checked') == false && $('#user_application_warrant_true').is(':checked') == false) {
+    var $toastContent = $("<span>Merci de remplir tous les champs pour pouvoir faire votre demande.</span>");
+    Materialize.toast($toastContent, 5000);
+    event.preventDefault();
+  } else if ($('#user_application_visit_needed_false').is(':checked') == false && $('#user_application_visit_needed_true').is(':checked') == false) {
+    var $toastContent = $("<span>Merci de remplir tous les champs pour pouvoir faire votre demande.</span>");
+    Materialize.toast($toastContent, 5000);
+    event.preventDefault();
+  }
+})
--- app/controllers/user_applications_controller.rb
+++ app/controllers/user_applications_controller.rb
@@ -10,7 +10,6 @@ class UserApplicationsController < ApplicationController
     @user_application = UserApplication.new(user_application_params)
     @flat = Flat.friendly.find(params[:flat_id])
     if params[:user_application]["warrant"][1].nil? || params[:user_application]["visit_needed"][1].nil?
-      flash[:notice] = "N'oubliez pas de remplir les cases vides"
       render :new
     else
       @user_application.warrant = ( params[:user_application]["warrant"][1] == "true" )
