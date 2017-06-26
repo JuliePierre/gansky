d6a25414005a50d9f4aaefcc4a2154d94e4f4148
Julie Pierre <julie.pierre3@gmail.com>
2017-06-26 15:45:50 +0200

    mobile quasi fini

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
   def confirmation(user_application)
     @user = user_application
 
-    attachments.inline['logo-gansky.png'] = File.read("#{Rails.root}/app/assets/images/logo-gansky.png")
+    # attachments.inline['logo-gansky.png'] = File.read("#{Rails.root}/app/assets/images/logo-gansky.png")
 
     mail(to: @user.applicant_email, subject: 'Votre dossier de réservation a bien été envoyé à Gansky')
   end
