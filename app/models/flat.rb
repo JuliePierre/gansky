8ac7878db6a04828429865f4752b78caadb79ba7
Julie Pierre <julie.pierre3@gmail.com>
2017-06-29 17:24:44 +0200

    modifs et seed pour pouvoir pusher sur heroku

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,11 +2,12 @@ class Flat < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   has_attachment :map_picture
-  has_attachments :photos, maximum: 6
+  has_attachment :cover_picture
+  has_attachments :photos
 
   has_many :flat_facilities, dependent: :destroy
   has_many :facilities, through: :flat_facilities
-  has_many :user_applications
+  has_many :user_applications, dependent: :destroy
 
   accepts_nested_attributes_for :flat_facilities
   accepts_nested_attributes_for :facilities
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,123 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# user = User.new(email: "test@me.com", password: "azerty", admin: true)
-# if user.save
-#   puts "Vous avez créé un admin"
-# else
-#   puts "Vous n'avez pas pu créé ce user"
-# end
+puts "Destroying all users"
+User.destroy_all
+alex = User.new(email: "amartin.essec@gmail.com", password: "azerty", admin: true)
+alex.save!
+puts "User Admin Alex créé"
+###########################
 
 puts "Destroying all facilities"
 Facility.destroy_all
 
+# Seed des facilities
 puts "Creating facilities"
-Facility.create(description: "Lits 2 places")
-Facility.create(description: "Terrasse aménagée")
-Facility.create(description: "Télévision")
-Facility.create(description: "Machine Nespresso")
-Facility.create(description: "Vaisselle")
-Facility.create(description: "Cuisine Equipée")
-Facility.create(description: "Congélateur")
-Facility.create(description: "Bureau")
-Facility.create(description: "Lave-vaisselle")
-Facility.create(description: "Lave-linge")
-Facility.create(description: "Console de jeu")
-Facility.create(description: "Sèche-linge")
-Facility.create(description: "Aspirateur")
-Facility.create(description: "Garage à vélos")
-Facility.create(description: "Ascenseur")
-Facility.create(description: "Four")
-Facility.create(description: "Place de parking")
-Facility.create(description: "Sèche-cheveux")
-Facility.create(description: "Home cinema")
-Facility.create(description: "Playstation 4")
-Facility.create(description: "Dressing")
-Facility.create(description: "Enceinte")
-Facility.create(description: "Vidéo-projecteur")
-Facility.create(description: "Baby-foot")
-Facility.create(description: "Fléchettes")
-Facility.create(description: "Arcade")
-Facility.create(description: "Ping-pong")
-Facility.create(description: "Four à micro-ondes")
-Facility.create(description: "Grille pain")
-Facility.create(description: "Appareil à croque-monsieur")
-Facility.create(description: "Bouilloire")
-Facility.create(description: "Fer à repasser")
-Facility.create(description: "Blender")
+lit = Facility.new(description: "Lits 2 places")
+lit.save!
+
+puts "first facility ok"
+
+terrasse = Facility.new(description: "Terrasse aménagée")
+terrasse.save!
+
+tv = Facility.new(description: "Télévision")
+tv.save!
+
+nespresso = Facility.new(description: "Machine Nespresso")
+nespresso.save!
+
+vaisselle = Facility.new(description: "Vaisselle")
+vaisselle.save!
+
+cuisine = Facility.new(description: "Cuisine Equipée")
+cuisine.save!
+
+congelateur = Facility.new(description: "Congélateur")
+congelateur.save!
+
+bureau = Facility.new(description: "Bureau")
+bureau.save!
+
+lave_vaisselle = Facility.new(description: "Lave-vaisselle")
+lave_vaisselle.save!
+
+lave_linge = Facility.new(description: "Lave-linge")
+lave_linge.save!
+
+console = Facility.new(description: "Console de jeu")
+console.save!
+
+seche_linge = Facility.new(description: "Sèche-linge")
+seche_linge.save!
+
+aspirateur = Facility.new(description: "Aspirateur")
+aspirateur.save!
+
+velo = Facility.new(description: "Garage à vélos")
+velo.save!
+
+ascenseur = Facility.new(description: "Ascenseur")
+ascenseur.save!
+
+four = Facility.new(description: "Four")
+four.save!
+
+parking = Facility.new(description: "Place de parking")
+parking.save!
+
+seche_cheveux = Facility.new(description: "Sèche-cheveux")
+seche_cheveux.save!
+
+home_cinema = Facility.new(description: "Home cinema")
+home_cinema.save!
+
+playstation = Facility.new(description: "Playstation 4")
+playstation.save!
+
+dressing = Facility.new(description: "Dressing")
+dressing.save!
+
+enceinte = Facility.new(description: "Enceinte")
+enceinte.save!
+
+video_projecteur = Facility.new(description: "Vidéo-projecteur")
+video_projecteur.save!
+
+baby_foot = Facility.new(description: "Baby-foot")
+baby_foot.save!
+
+flechettes = Facility.new(description: "Fléchettes")
+flechettes.save!
+
+arcade = Facility.new(description: "Arcade")
+arcade.save!
+
+ping_pong = Facility.new(description: "Ping-pong")
+ping_pong.save!
+
+micro_ondes = Facility.new(description: "Four à micro-ondes")
+micro_ondes.save!
+
+grille_pain = Facility.new(description: "Grille pain")
+grille_pain.save!
+
+croque_monsieur = Facility.new(description: "Appareil à croque-monsieur")
+croque_monsieur.save!
+
+bouilloire = Facility.new(description: "Bouilloire")
+bouilloire.save!
+
+fer = Facility.new(description: "Fer à repasser")
+fer.save!
+
+blender = Facility.new(description: "Blender")
+blender.save!
+#########################
+
+# Seed des appartements
+puts "Suppression des appartements"
+Flat.destroy_all
+puts "Création des 2 appartements"
+
+dagobah = Flat.new(name: "Dagobah", address: "9 Les Larris Orange", zipcode: "95300", city: "Pontoise", neighborhood: "Cergy-Préfecture", description: "Magnifique appartement comprenant 4 chambres, 2 salles de bain, une cuisine et un salon-salle à manger. Tout y est entièrement meublé et équipé (voir liste des équipements ci-dessous).", sub_description: "Appartement idéalement situé, proche de l'Essec, du centre commercial des 3 Fontaines et de la gare RER.", nb_rooms: 4, price_per_room: 590.0, occupied: false, availability_date: "1er août 2017")
+dagobah.save!
+naboo = Flat.new(name: "Naboo", address: "8 Les Larris Orange", zipcode: "95300", city: "Pontoise", neighborhood: "Cergy-Préfecture", description: "Magnifique appartement comprenant 4 chambres, 2 salles de bain, une cuisine et un salon-salle à manger. Tout y est entièrement meublé et équipé (voir liste des équipements ci-dessous).", sub_description: "Appartement idéalement situé, proche de l'Essec, du centre commercial des 3 Fontaines et de la gare RER.", nb_rooms: 4, price_per_room: 590.0, occupied: false, availability_date: "1er août 2017")
+naboo.save!
