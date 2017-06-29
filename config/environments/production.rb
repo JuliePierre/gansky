54b90b815f167cdf6e416e54ecb0435e24dba967
Julie Pierre <julie.pierre3@gmail.com>
2017-06-29 17:40:22 +0200

    changement d'adresse url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "gansky.fr" }
+  config.action_mailer.default_url_options = { host: "gansky.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
