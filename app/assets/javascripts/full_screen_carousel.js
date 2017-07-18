c4bb5ed05767cfa68826367372e2b3e8b26f05d3
Julie Pierre <julie.pierre3@gmail.com>
2017-07-18 12:22:17 +0200

    ajout d'un zoom sur les photos du carousel

---
+++ app/assets/javascripts/full_screen_carousel.js
@@ -0,0 +1,8 @@
+$('#close-full-screen-carousel').click(function(){
+  $('#full-carousel-example-generic').addClass('hidden');
+  $('#full-wrapper-for-full-screen-carousel').addClass('hidden');
+})
+$('.open-full-screen-carousel').click(function(){
+  $('#full-carousel-example-generic').removeClass('hidden');
+  $('#full-wrapper-for-full-screen-carousel').removeClass('hidden');
+})
