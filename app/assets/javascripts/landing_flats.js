c420874988c49ec6dd9a385a06d6403688ade802
Julie Pierre <julie.pierre3@gmail.com>
2017-07-19 19:11:38 +0200

    modif de la navbar

--- app/assets/javascripts/landing_flats.js
+++ app/assets/javascripts/landing_flats.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#see-flats-button').click(function(event) {
+  $('.see-flats-button').click(function(event) {
     console.log('test');
     var targetPosition = $('#landing-flats').offset().top;
     $('html, body').animate({ scrollTop: targetPosition }, 'slow');
