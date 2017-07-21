b4b2d4d988e88a0d98bfe04a628264212fe87584
Julie Pierre <julie.pierre3@gmail.com>
2017-07-21 18:46:08 +0200

    ordre d'apparition des appartements sur la page d'accueil ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,9 @@ class FlatsController < ApplicationController
   attr_reader :formated_date
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @flats = Flat.all
+    @available_flats = Flat.where("occupied = ? ", false).order(availability_date: :asc)
+    @occupied_flats = Flat.where("occupied = ? ", true)
+    @flats = [@available_flats, @occupied_flats].flatten
     @carte_cergy = Medium.all.first
   end
 
